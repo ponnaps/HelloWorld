bed2e6523404276daed1c084822c3ccf11b72f26
ponnaps <harish.ponnapureddy@gmail.com>
2014-02-08 13:55:46 -0600

    Version 0.1

---
+++ script.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+use CGI; 
+print CGI->header('text/html'); 
+print "hello world \n";
+
+Shell Scripting
+Python
+Apache Web Server
+
+
+http://git-scm.com/book/en/Getting-Started-About-Version-Control
+http://maven.apache.org/guides/getting-started/maven-in-five-minutes.html
+http://jenkins-ci.org/
+http://git-scm.com/book/en/Getting-Started-Git-Basics
+https://help.github.com/articles/set-up-git
+
+
+AWS Basics
+
+
+Git Questions 
+****************
+
+Git stores the entire copy of the project locally?
+Git checks the difference locally. How does it do that? where is the history stored ?
+On COMMIT, data is stored locally? Not on the GITHUB server? Page 14
+Where is ths STAGING area stored?
